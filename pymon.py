8413aa1a8934c704229bae49d2bc3ddfe213c405
Skyneo <skyneo@live.fr>
2015-03-31 22:02:40 +0200

    Update pymon.py
    -Ajout d'un message au début du programme, avec un nota bene et un copyright.
    -Déplacement de la fonction du son + de la ligne de l'importation de la librairie winsound.
    -Ajout de couleurs sur les boutons, c'est plus joli (et ça change quand on clique dessus, wouhou)
           -création de variables string pour certaines couleurs en hexadécimal
    -Débug du launcher a l'aide d'une sous-fonction qui code pour deux commandes. Avant, si on fermait le launcher manuellement, le menu se lançait mais sans avoir défini l'OS, ce qui était problématique car le programme entier allait buguer. Maintenant, l'utilisateur ne peut pas accéder au menu sans choisir d'OS. Si on ferme la fenêtre manuellement, le menu ne s'ouvre pas et le programme s'arrête.
    -Suppression de la ligne qui codait pour la taille du launcher, elle servait a rien.
    -Ajout de commentaires sur la fonction des meilleurs scores
    ----------------------------
    Autre chose : on peut choisir une couleur de fond de la fenêtre, j'y réfléchis.

--- pymon.py
+++ pymon.py
@@ -1,9 +1,26 @@
+##Petit message :
+#Pymon (c) Antoine Bouquin et Quentin Albertone. Bienvenue dans le code source. Vous avez le droit de le modifier et/ou de le redistribuer en précisant que l'original vient de nous et sans le vendre. Merci.
+
+#-------------- Nota bene :-----------------------
+#Le programme est conçu pour tourner sous Windows et UNIX. Les tailles des fenêtres sont conçues pour avoir la dimension parfaite pour Windows 7/Aero désactivé. Ainsi, suivant l'OS/le thème, l'agencement peut ne pas être parfait. Cependant, cela est purement esthétique et n'affecte en rien le fonctionnement global du programme. Bon jeu !
+
 ##Importation des modules
 
 from tkinter import * #Interface
 from random import * #Aleatoire
+from winsound import * #Lecture des sons
+
+##Strings pour une couleur hexadécimale (purement esthétique, pour les boutons)
+grisClair="#dedede"
+grisFonce="#3d3d3d"
+vertClair="#2fab3d"
+jauneClair="#fffd47"
+orangeClair="#ffab48"
+rougeClair="#ff2e2e"
+bleuClair="#5367ff"
 
 ##Définitions des fonctions permettant de définir l'OS de l'utilisateur
+
 def OSchosenWin():
     OSWin=True
     return OSWin
@@ -20,23 +37,26 @@ def launch():
     #Définition de la fenêtre
     launcher=Tk()
     launcher.title("Pymon launcher")
-    launcher.geometry("250x75")
+    
+    def launcherClose():   #Afin de fermer la fenêtre en ouvrant le menu, on doit définir une fonction codant pour ça, sinon on doit
+        launcher.destroy() #coder la fermeture de la fenêtre en tant que commande sur le bouton, mais dans ce cas là, le menu doit s'ouvrir
+        menu()             #en fin de fonction car on ne peut pas mettre plus de deux commandes sur un bouton.
+                           #On pouvait donc quitter le launcher sans choisir d'OS et le programme se lance quand même.
     
     #Définition des boutons. Ils doivent prendre toute la fenêtre donc pack() est plus utile
     OSquestion=Label(launcher,text="Bienvenue dans Pymon ! Choisissez votre OS :",anchor=CENTER,justify=CENTER)
     OSquestion.pack(fill=BOTH)
     
-    unixButton=Button(launcher,text="Windows",command=OSchosenWin and launcher.destroy)
-    unixButton.pack(fill=BOTH)
-    
-    winButton=Button(launcher,text="UNIX",command=OSchosenUnix and launcher.destroy)
+    unixButton=Button(launcher,text="Windows",command=OSchosenWin and launcherClose,bg="blue",activebackground=bleuClair) 
+    unixButton.pack(fill=BOTH)                                                                 #Ainsi, l'utilisateur ne peut pas lancer le
+                                                                                               #programme sans choisir d'OS. Il peut quitter
+                                                                                               #le launcher, mais dans ce cas le programme
+                                                                                               #s'arrête.
+    winButton=Button(launcher,text="UNIX",command=OSchosenUnix and launcherClose,bg="orange",activebackground=orangeClair)
     winButton.pack(fill=BOTH)
     
     #Lancement du launcher
     launcher.mainloop()
-    
-    #Lancement du menu
-    menu()
 
 ##Création de l'interface graphique du Menu principal
 
@@ -45,37 +65,37 @@ def menu():
     #Création de la fenêtre
     menuWindow=Tk()
     menuWindow.title("Pymon")
-    menuWindow.geometry("350x290")
+    menuWindow.geometry("350x265")
     
     #Création de chaque bouton
     title=Label(menuWindow,text="Bienvenue dans Pymon !",anchor=CENTER,justify=CENTER)
     title.pack(fill=BOTH)
     
-    tuto=Button(menuWindow,text="Tutoriel")
+    tuto=Button(menuWindow,text="Tutoriel",bg="green",activebackground=vertClair)
     tuto.pack(fill=BOTH)
     
     lvs=Label(menuWindow,text="-=-=-=-=- Niveaux -=-=-=-=-",anchor=CENTER,justify=CENTER)
     lvs.pack(fill=BOTH)
     
-    lv1=Button(menuWindow,text="Niveau 1")
+    lv1=Button(menuWindow,text="Niveau 1",bg="yellow",activebackground=jauneClair)
     lv1.pack(fill=BOTH)
     
-    lv2=Button(menuWindow,text="Niveau 2")
+    lv2=Button(menuWindow,text="Niveau 2",bg="orange",activebackground=orangeClair)
     lv2.pack(fill=BOTH)
     
-    lv3=Button(menuWindow,text="Niveau 3")
+    lv3=Button(menuWindow,text="Niveau 3",bg="red",activebackground=rougeClair)
     lv3.pack(fill=BOTH)
     
     danger=Label(menuWindow,text="!!!! DANGER !!!!",anchor=CENTER,justify=CENTER,fg="red")
     danger.pack(fill=BOTH)
     
-    survival=Button(menuWindow,text="Survival !")
+    survival=Button(menuWindow,text="Survival !",bg="black",fg="white",activebackground=grisFonce)
     survival.pack(fill=BOTH)
     
     separation=Label(menuWindow,text="-=-=-=-=-=-=-=-=-=-=-=-=-",anchor=CENTER,justify=CENTER)
     separation.pack(fill=BOTH)
     
-    menuBest=Button(menuWindow,text="Meilleurs scores",command=bestScore)
+    menuBest=Button(menuWindow,text="Meilleurs scores",command=bestScore,bg="grey",activebackground=grisClair)
     menuBest.pack(fill=BOTH)
     
     commands=Label(menuWindow,text="Touches : QSDFJKL",fg='grey')
@@ -90,65 +110,7 @@ def menu():
 ## Choix aléatoire d'un nombre puis d'une note
 
 def note():
-    note=['do.mp3','re.mp3','mi.mp3','fa.mp3','sol.mp3','la.mp3','si.mp3']
-    clavier=['q','s','d','f','j','k','l']
-    c=randrange(6)
-    keyboard=clavier[c]
-    
-    #fin fonction programme - Début aide diagnostique
-    
-    print (c)
-    print(note[c])
-    print(keyboard)
-    
-    # Réponse de l'utilisateur
-    
-    r=input('donner la lettre correspondant à cett note : ')
-    if keyboard==r :
-        print('bravo')
-    else :
-        print('hiiinnn faux')
-
-##Définition de la fonction codant pour les meilleurs scores du Survival
-
-def bestScore():
-    
-    best=open("best.txt","r")
-    highscore=best.readlines()
-    
-    displayHS=Tk()
-    displayHS.title("Pymon - Meilleurs scores")
-    
-    phraseHS=Label(displayHS,text="Ceci sont les meilleurs scores obtenus par les joueurs sur le Survival :")
-    phraseHS.pack(fill=BOTH)
-    
-    printHighscore=Listbox(displayHS)
-    
-    listHS=len(highscore)
-    i=0
-    
-    for i in range(listHS):
-        printHighscore.insert(i,highscore[i])
-        i+=1
-    printHighscore.pack(fill=BOTH)
-    
-    menuBack=Button(displayHS,text="Retour au menu",command=menu and displayHS.destroy)
-    menuBack.pack(fill=BOTH)
-    
-    best.close()
-    
-    displayHS.mainloop()
-    
-########################
-#Lancement du programme#
-########################
-launch()
-
-###### ajouts Quentin ######
-import winsound
-
-def note():
-    note=['do.wav','re.wav','mi.wav','fa.wav','sol.wav','la.wav','si.wav']
+    note=['do.wav','re.wav','mi.wav','fa.wav','sol.wav','la.wav','si.wav'] #TODO : corriger les chemins
     clavier=['q','s','d','f','j','k','l']
     c=randrange(6)
     keyboard=clavier[c]
@@ -164,8 +126,50 @@ def note():
     
     reponse=input('donner la lettre correspondant à cett note : ')
     if keyboard==r :
-        print('bravo')
+        print('bravo') #TODO : faire une fenêtre, c'est mieux
     else :
         print('hiiinnn faux')
+    
+    #TODO : ajouter dans la liste la touche et le son, histoire d'avoir la série de notes.
 
-######              ######
+##Définition de la fonction codant pour les meilleurs scores du Survival
+
+def bestScore():
+    
+    #Ouverture du fichier
+    best=open("best.txt","r")
+    highscore=best.readlines() #"highscore" est une liste qui compte chaque ligne comme un élément de la liste
+    
+    #Création de la fenêtre
+    displayHS=Tk()
+    displayHS.title("Pymon - Meilleurs scores")
+    
+    phraseHS=Label(displayHS,text="Ceci sont les meilleurs scores obtenus par les joueurs sur le Survival :")
+    phraseHS.pack(fill=BOTH)
+    
+    #Création de la listbox pour afficher les scores
+    printHighscore=Listbox(displayHS)
+    
+    listHS=len(highscore) #Définition de la variable correspondant a la longueur de la liste highscore
+    i=0
+    
+    #Boucle pour afficher chaque élément de highscore dans la listbox
+    for i in range(listHS):
+        printHighscore.insert(i,highscore[i])
+        i+=1
+    printHighscore.pack(fill=BOTH)
+    
+    #Bouton de retour au menu
+    menuBack=Button(displayHS,text="Retour au menu",command=menu and displayHS.destroy,bg="grey",activebackground=grisClair)
+    menuBack.pack(fill=BOTH)
+    
+    #Fermeture du fichier
+    best.close()
+    
+    #Lancement de la fenêtre
+    displayHS.mainloop()
+    
+########################
+#Lancement du programme#
+########################
+launch()
